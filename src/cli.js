166727c62015361eaa5aff7a580915d6518b1e54
Anna Yastrebova <direct.yastrebova@gmail.com>
2023-07-25 19:47:18 +0200

    third step

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+// src/cli.js
+import readlineSync from 'readline-sync';
+
+export function greetUser() {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
