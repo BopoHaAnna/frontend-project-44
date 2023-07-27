9981f4b7267576494d9c9e9058a9ddd655dd73ea
Anna Yastrebova <direct.yastrebova@gmail.com>
2023-07-27 17:13:42 +0200

    fifth step 2.0

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
+import playBrainEven from '../src/index.js';
 
-const isEven = (number) => number % 2 === 0;
-const playBrainEven = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = greetUser();
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-  const maxRounds = 3;
-  let correctCount = 0; // Переменная для подсчета правильных ответов
-  for (let i = 0; i < maxRounds; i += 1) {
-    const randomNumber = Math.floor(Math.random() * 100); // рандомное число
-    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no'; // правильный ответ
-    console.log(`Question: ${randomNumber}`); // задаем вопрос
-    const userAnswer = readlineSync.question('Your answer: '); // ответ пользователя
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-      correctCount += 1; // Увеличиваем счетчик правильных ответов
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${name}!`);
-      break; // Прерываем цикл при неверном ответе
-    }
-  }
-
-  if (correctCount === maxRounds) {
-    console.log(`Congratulations, ${name}!`);
-  }
-};
 playBrainEven();
